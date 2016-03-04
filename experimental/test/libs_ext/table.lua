360d7ee3ffbfd354df5f0814cfdd5ccd71a1341b
Lesley De Cruz <lesley.decruz+git@gmail.com>
2016-03-05 00:05:48 +0100

    Reorganise meta_pairs.lua into libs/core and libs_ext/table

--- experimental/test/libs_ext/table.lua
+++ experimental/test/libs_ext/table.lua
@@ -35,3 +35,30 @@ do
     assert(t.n == 3 and t[0] == nil and t[1] == nil and t[2] == nil and t[3] == nil and t[4] == nil)
   end
 end
+
+--- ipairs and pairs metamethods
+-- pairs and ipairs work as expected when the corresponding metamethods
+-- are set to the stateless iterators with the original behaviour.
+-- +lua52 +__ipairs +__pairs
+do
+  if os.getenv("LUA52") then
+    local function iter(t, i)
+      i = i + 1
+      if t[i] then return i, t[i]+2 end
+    end
+    local function itergen(t)
+      return iter, t, 0
+    end
+    local t = setmetatable({}, { __pairs = itergen, __ipairs = itergen })
+    for i=1,10 do t[i] = i+100 end
+    local a, b = 0, 0
+    for j=1,100 do for k,v in ipairs(t) do a = a + k; b = b + v end end
+    assert(a == 5500)
+    assert(b == 107500)
+    a, b = 0, 0
+    for j=1,100 do for k,v in pairs(t) do a = a + k; b = b + v end end
+    assert(a == 5500)
+    assert(b == 107500)
+  end
+end
+
