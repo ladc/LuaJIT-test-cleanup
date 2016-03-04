6a406446fca4fd91a993edc8236f83fb6db952cd
Lesley De Cruz <lesley.decruz+git@gmail.com>
2016-03-04 23:55:15 +0100

    Add comments to meta_call.lua

--- experimental/test/libs/table/meta_call.lua
+++ experimental/test/libs/table/meta_call.lua
@@ -1,3 +1,5 @@
+-- Tests for the +__call metamethod
+-- +setmetatable +meta
 
 local function callmeta(o, a, b)
   return o, a, b
@@ -5,6 +7,7 @@ end
 
 local meta = { __call = callmeta }
 
+--- call metamethod on tables correctly passes arguments
 do
   local t = setmetatable({}, meta)
   local o,a,b = t()
@@ -15,6 +18,8 @@ do
   assert(o == t and a == "foo" and b == "bar")
 end
 
+--- call metamethod on newproxy object correctly passes arguments
+-- +newproxy
 do
   local u = newproxy(true)
   getmetatable(u).__call = callmeta
@@ -27,15 +32,22 @@ do
   assert(o == u and a == "foo" and b == "bar")
 end
 
-debug.setmetatable(0, meta)
-local o,a,b = (42)()
-assert(o == 42 and a == nil and b == nil)
-local o,a,b = (42)("foo")
-assert(o == 42 and a == "foo" and b == nil)
-local o,a,b = (42)("foo", "bar")
-assert(o == 42 and a == "foo" and b == "bar")
-debug.setmetatable(0, nil)
+--- call metamethod on number correctly passes arguments
+-- +debug
+do
+  debug.setmetatable(0, meta)
+  local o,a,b = (42)()
+  assert(o == 42 and a == nil and b == nil)
+  local o,a,b = (42)("foo")
+  assert(o == 42 and a == "foo" and b == nil)
+  local o,a,b = (42)("foo", "bar")
+  assert(o == 42 and a == "foo" and b == "bar")
+  debug.setmetatable(0, nil)
+end
 
+--- __call in __add metamethod propagation
+-- Check if the metamethod __call is correctly propagated when its table is
+-- defined as the __add metamethod for another table.
 do
   local tc = setmetatable({}, { __call = function(o,a,b) return o end})
   local ta = setmetatable({}, { __add = tc})
@@ -45,29 +57,41 @@ do
   getmetatable(tc).__call = function(o,a,b) return a end
   local o,a = ta + ta
   assert(o == ta and a == nil)
+  
+  getmetatable(tc).__call = function(o,a,b) return b end
+  local o,a = ta + ta
+  assert(o == ta and a == nil)
 end
 
+--- call metamethod arithmetic in loop
 do
   local t = setmetatable({}, { __call = function(t, a) return 100-a end })
   for i=1,100 do assert(t(i) == 100-i) end
 end
 
+--- if call metamethod is rawget, find index behaviour
 do
   local t = setmetatable({}, { __call = rawget })
   for i=1,100 do t[i] = 100-i end
   for i=1,100 do assert(t(i) == 100-i) end
 end
 
-debug.setmetatable(0, { __call = function(n) return 100-n end })
-for i=1,100 do assert((i)() == 100-i) end
-debug.setmetatable(0, nil)
+--- call metamethod on number metatable passes number value as argument
+-- +debug
+do
+  debug.setmetatable(0, { __call = function(n) return 100-n end })
+  for i=1,100 do assert((i)() == 100-i) end
+  debug.setmetatable(0, nil)
+end
 
+--- if call is rawset and newindex is pcall, find newindex assignment behaviour
 do
   local t = setmetatable({}, { __newindex = pcall, __call = rawset })
   for i=1,100 do t[i] = 100-i end
   for i=1,100 do assert(t[i] == 100-i) end
 end
 
+---
 do
   local t = setmetatable({}, {
     __index = pcall, __newindex = rawset,
